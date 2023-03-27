b0ee2cb3a4a324ff86d5addbfad4a9b1b47da940
BanPei Lim <banpei.lim@gmail.com>
2023-03-27 13:20:56 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/on", async (req, res) => {
+  try {
+    const response = await axios.put(
+      "http://192.168.1.113/api/HIjG0NftLDjrWmp45y-8Ew7u0HrOKXGz6MgQmBIA/lights/1/state",
+      { on: true }
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending PUT request");
+  }
+});
+
+app.get("/off", async (req, res) => {
+  try {
+    const response = await axios.put(
+      "http://192.168.1.113/api/HIjG0NftLDjrWmp45y-8Ew7u0HrOKXGz6MgQmBIA/lights/1/state",
+      { on: false }
+    );
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending PUT request");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
